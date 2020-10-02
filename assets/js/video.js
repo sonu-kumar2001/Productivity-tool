c46c9caebec396e312b504209c57d69984195283
Abhijeet Burman <abhiburman20@gmail.com>
2020-10-02 10:22:46 +0530

    video

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,19 +1,20 @@
-// let videoProgress = document.querySelector(".mp4");
-
-// let start = 0;
-// let duration = 0;
-
-// // Set video element to variable
-// // var video = document.getElementById('player1');
-
-// // var videoStartTime = 0;
-// // var durationTime = 0;
-
-// // video.addEventListener('loadedmetadata', function() {
-// //   videoStartTime = 2;
-// //   durationTime = 4;
-// //   this.currentTime = videoStartTime;
-// // }, false);
-// function handleVideo (event){
-// }
-// videoProgress.addEventListener('loadedmetadata', handleVideo);
+let videoProgress = document.querySelector(".mp4");
+let start = 0;
+let duration = 0;
+// Set video element to variable
+// var video = document.getElementById('player1'
+// var videoStartTime = 0;
+// var durationTime = 
+// video.addEventListener('loadedmetadata', function() {
+//   videoStartTime = 2;
+//   durationTime = 4;
+//   this.currentTime = videoStartTime;
+// }, false);
+function handleVideo (event){
+    videoDuration = event.target.duration;
+    pertaskDuration = videoDuration / userInfo.length;
+    console.log(videoDuration);
+    console.log(userInfo.length);
+    console.log(pertaskDuration);
+}
+videoProgress.addEventListener('loadedmetadata', handleVideo);
