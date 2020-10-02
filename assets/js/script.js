44db93480631ea6fb471436cc10c3609a66467ff
sonu-kumar2001 <sonurocckkerr@gmail.com>
2020-10-02 14:58:10 +0530

    solved merge conflicts

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ let form = document.querySelector('form');
 let ul = document.querySelector('.list-item');
 let root = document.querySelector('.root');
 
-
 let userInfo = JSON.parse(localStorage.getItem("userInfo")) || [];
 
 let taskName = document.querySelector('.taskname');
@@ -124,51 +123,6 @@ function decS() {
 setInterval(pomodoroTimer, 1800000);
 
 // creating ui dynamically
-function createUi(arr) {
-    if(userInfo.length > 4) return
-    ul.innerHTML = ''
-    arr.forEach((element,index) => {
-        let li = document.createElement('li');
-        li.classList.add('flex-between');
-        let div1 = document.createElement('div');
-        div1.classList.add('info-flex');
-        let p1 = document.createElement('p');
-        p1.innerText = element.name;
-        p1.classList.add('taskName');
-        let div2 = document.createElement('div');
-        div2.classList.add('button','flex');
-        let p2 = document.createElement('p');
-        p2.innerText = `${element.hour} : ${element.min}: ${element.second}`;
-        p2.classList.add('taskTime');
-        let p3 = document.createElement('p');
-        p3.classList.add('play','Bttn');
-        p3.innerHTML = `<i class="far fa-play-circle"></i>`;
-        let p4 = document.createElement('p');
-        p4.classList.add('close','Bttn');
-        p4.innerHTML = `<i class="fas fa-times-circle"></i>`;
-        //added event listener on close icon
-        p4.addEventListener('click',deleteHandle);
-        p4.setAttribute("data-id", index);
-
-       div1.append(p1);
-       div2.append(p2,p3,p4);
-       li.append(div1,div2);
-       ul.append(li);
-         // adding event listener on play button
-        li.addEventListener('click', function(event){
-            if(event.target.classList.contains('fa-play-circle')) {
-                let index = [...ul.children].indexOf(li);
-                userInfo[index].clicked += 1;
-                if(userInfo[index].clicked % 2 != 0) {
-                    function startInterval(){
-                        startTimer = setInterval(function() {
-                            timer(li.querySelector('.taskTime'),[...ul.children].indexOf(li));
-                        }, 1000);
-                    }
-                    startInterval();
-                } else {
-                    clearInterval(startTimer);
-                }
 
 function createUi(arr) {
   if (userInfo.length > 4) return;
